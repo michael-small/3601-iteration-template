9743cf00bc9a3f7dc25ba094a7dd2f73c9355ca5
Michael Small <chickenhat22@gmail.com>
2024-01-28 16:55:29 -0600

    Pull users from `json-server` (installed globally for now)

--- client/src/app/users/user-profile.component.ts
+++ client/src/app/users/user-profile.component.ts
@@ -10,15 +10,15 @@ import { UserCardComponent } from './user-card.component';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
-    selector: 'app-user-profile',
-    templateUrl: './user-profile.component.html',
-    styleUrls: ['./user-profile.component.scss'],
-    standalone: true,
-    imports: [UserCardComponent, MatCardModule]
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss'],
+  standalone: true,
+  imports: [UserCardComponent, MatCardModule],
 })
 export class UserProfileComponent implements OnInit, OnDestroy {
   user: User;
-  error: { help: string, httpResponse: string, message: string };
+  error: { help: string; httpResponse: string; message: string };
 
   // This `Subject` will only ever emit one (empty) value when
   // `ngOnDestroy()` is called, i.e., when this component is
@@ -26,64 +26,42 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   // terminate, allowing the system to free up their resources (like memory).
   private ngUnsubscribe = new Subject<void>();
 
-  constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private userService: UserService) { }
+  constructor(
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
+  // TODO - add comments back
   ngOnInit(): void {
-    // The `map`, `switchMap`, and `takeUntil` are all RXJS operators, and
-    // each represents a step in the pipeline built using the RXJS `pipe`
-    // operator.
-    // The map step takes the `ParamMap` from the `ActivatedRoute`, which
-    // is typically the URL in the browser bar.
-    // The result from the map step is the `id` string for the requested
-    // `User`.
-    // That ID string gets passed (by `pipe`) to `switchMap`, which transforms
-    // it into an Observable<User>, i.e., all the (zero or one) `User`s
-    // that have that ID.
-    // The `takeUntil` operator allows this pipeline to continue to emit values
-    // until `this.ngUnsubscribe` emits a value, saying to shut the pipeline
-    // down and clean up any associated resources (like memory).
-    this.route.paramMap.pipe(
-      // Map the paramMap into the id
-      map((paramMap: ParamMap) => paramMap.get('id')),
-      // Maps the `id` string into the Observable<User>,
-      // which will emit zero or one values depending on whether there is a
-      // `User` with that ID.
-      switchMap((id: string) => this.userService.getUserById(id)),
-      // Allow the pipeline to continue to emit values until `this.ngUnsubscribe`
-      // returns a value, which only happens when this component is destroyed.
-      // At that point we shut down the pipeline, allowed any
-      // associated resources (like memory) are cleaned up.
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe({
-      next: user => {
-        this.user = user;
-        return user;
-      },
-      error: _err => {
-        this.error = {
-          help: 'There was a problem loading the user – try again.',
-          httpResponse: _err.message,
-          message: _err.error?.title,
-        };
-      }
-      /*
-       * You can uncomment the line that starts with `complete` below to use that console message
-       * as a way of verifying that this subscription is completing.
-       * We removed it since we were not doing anything interesting on completion
-       * and didn't want to clutter the console log
-       */
-      // complete: () => console.log('We got a new user, and we are done!'),
-    });
+    this.route.paramMap
+      .pipe(
+        map((paramMap: ParamMap) => {
+          console.log(paramMap);
+          return paramMap.get('id');
+        }),
+        switchMap((id: string) => this.userService.getUserById(id)),
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe({
+        next: (user) => {
+          this.user = user;
+          return user;
+        },
+        error: (_err) => {
+          this.error = {
+            help: 'There was a problem loading the user – try again.',
+            httpResponse: _err.message,
+            message: _err.error?.title,
+          };
+        },
+        // complete: () => console.log('We got a new user, and we are done!'),
+      });
   }
 
+  // TODO - add comments back
   ngOnDestroy() {
-    // When the component is destroyed, we'll emit an empty
-    // value as a way of saying that any active subscriptions should
-    // shut themselves down so the system can free up any associated
-    // resources, like memory.
     this.ngUnsubscribe.next();
-    // Calling `complete()` says that this `Subject` is done and will
-    // never send any further values.
     this.ngUnsubscribe.complete();
   }
 }
--- client/src/app/users/user.service.ts
+++ client/src/app/users/user.service.ts
@@ -10,11 +10,12 @@ import { map } from 'rxjs/operators';
  * about `Users` from the server.
  */
 @Injectable({
-  providedIn: `root`
+  providedIn: `root`,
 })
 export class UserService {
   // The URL for the users part of the server API.
   readonly userUrl: string = `${environment.apiUrl}users`;
+  readonly userUrlMichael: string = `http://localhost:3000/users`;
 
   private readonly roleKey = 'role';
   private readonly ageKey = 'age';
@@ -27,8 +28,7 @@ export class UserService {
   // of `HttpClient` in the unit tests so they don't have to
   // make "real" HTTP calls to a server that might not exist or
   // might not be currently running.
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   /**
    * Get all the users from the server, filtered by the information
@@ -47,7 +47,11 @@ export class UserService {
    *  from the server after a possibly substantial delay (because we're
    *  contacting a remote server over the Internet).
    */
-  getUsers(filters?: { role?: UserRole; age?: number; company?: string }): Observable<User[]> {
+  getUsers(filters?: {
+    role?: UserRole;
+    age?: number;
+    company?: string;
+  }): Observable<User[]> {
     // `HttpParams` is essentially just a map used to hold key-value
     // pairs that are then encoded as "?key1=value1&key2=value2&…" in
     // the URL when we make the call to `.get()` below.
@@ -65,7 +69,7 @@ export class UserService {
     }
     // Send the HTTP GET request with the given URL and parameters.
     // That will return the desired `Observable<User[]>`.
-    return this.httpClient.get<User[]>(this.userUrl, {
+    return this.httpClient.get<User[]>(this.userUrlMichael, {
       params: httpParams,
     });
   }
@@ -78,7 +82,7 @@ export class UserService {
    */
   getUserById(id: string): Observable<User> {
     // The input to get could also be written as (this.userUrl + '/' + id)
-    return this.httpClient.get<User>(`${this.userUrl}/${id}`);
+    return this.httpClient.get<User>(`${this.userUrlMichael}/${id}`);
   }
 
   /**
@@ -94,19 +98,27 @@ export class UserService {
    * @param filters the map of key-value pairs used for the filtering
    * @returns an array of `Users` matching the given filters
    */
-  filterUsers(users: User[], filters: { name?: string; company?: string }): User[] { // skipcq: JS-0105
+  filterUsers(
+    users: User[],
+    filters: { name?: string; company?: string }
+  ): User[] {
+    // skipcq: JS-0105
     let filteredUsers = users;
 
     // Filter by name
     if (filters.name) {
       filters.name = filters.name.toLowerCase();
-      filteredUsers = filteredUsers.filter(user => user.name.toLowerCase().indexOf(filters.name) !== -1);
+      filteredUsers = filteredUsers.filter(
+        (user) => user.name.toLowerCase().indexOf(filters.name) !== -1
+      );
     }
 
     // Filter by company
     if (filters.company) {
       filters.company = filters.company.toLowerCase();
-      filteredUsers = filteredUsers.filter(user => user.company.toLowerCase().indexOf(filters.company) !== -1);
+      filteredUsers = filteredUsers.filter(
+        (user) => user.company.toLowerCase().indexOf(filters.company) !== -1
+      );
     }
 
     return filteredUsers;
@@ -114,6 +126,8 @@ export class UserService {
 
   addUser(newUser: Partial<User>): Observable<string> {
     // Send post request to add a new user with the user data as the body.
-    return this.httpClient.post<{id: string}>(this.userUrl, newUser).pipe(map(res => res.id));
+    return this.httpClient
+      .post<{ id: string }>(this.userUrl, newUser)
+      .pipe(map((res) => res.id));
   }
 }
