d3a65646f74c8e03601010a70cdd177c2a86f2c8
Michael Small <chickenhat22@gmail.com>
2024-01-28 20:29:46 -0600

    Compare signal vs observable benefits

--- client/src/app/users/user-profile.component.ts
+++ client/src/app/users/user-profile.component.ts
@@ -11,9 +11,15 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from './user';
 import { UserService } from './user.service';
 import { Subject, of } from 'rxjs';
-import { catchError, map, switchMap, takeUntil } from 'rxjs/operators';
+import {
+  catchError,
+  map,
+  startWith,
+  switchMap,
+  takeUntil,
+} from 'rxjs/operators';
 import { UserCardComponent } from './user-card.component';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { MatCardModule } from '@angular/material/card';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 
@@ -22,24 +28,47 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
   standalone: true,
-  imports: [UserCardComponent, MatCardModule, JsonPipe, AsyncPipe],
+  imports: [UserCardComponent, MatCardModule, AsyncPipe],
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent {
   user: User;
   error: { help: string; httpResponse: string; message: string };
 
-  $id = input<string>('', { alias: 'id' });
-  $user = computed(() => {
-    return this.userService.getUserById(this.$id()).pipe(
-      catchError((_err) =>
-        of({
+  exampleAsyncPipeAccessProperty = '{{ (user$ | async).name }}';
+
+  $user = toSignal(
+    this.route.paramMap.pipe(
+      map((paramMap: ParamMap) => {
+        return paramMap.get('id');
+      }),
+      switchMap((id: string) => this.userService.getUserById(id)),
+      catchError((_err) => {
+        this.error = {
           help: 'There was a problem loading the user – try again.',
           httpResponse: _err.message,
           message: _err.error?.title,
-        })
-      )
-    );
-  });
+        };
+        return of<User>(undefined);
+      }),
+      takeUntilDestroyed(this.destroy)
+    )
+  );
+
+  user$ = this.route.paramMap.pipe(
+    map((paramMap: ParamMap) => {
+      return paramMap.get('id');
+    }),
+    switchMap((id: string) => this.userService.getUserById(id)),
+    catchError((_err) => {
+      this.error = {
+        help: 'There was a problem loading the user – try again.',
+        httpResponse: _err.message,
+        message: _err.error?.title,
+      };
+      return of<User>(undefined);
+    }),
+    takeUntilDestroyed(this.destroy)
+  );
 
   constructor(
     private snackBar: MatSnackBar,
@@ -47,31 +76,4 @@ export class UserProfileComponent implements OnInit {
     private userService: UserService,
     private destroy: DestroyRef
   ) {}
-
-  // TODO - add comments back
-  ngOnInit(): void {
-    this.route.paramMap
-      .pipe(
-        map((paramMap: ParamMap) => {
-          console.log(paramMap);
-          return paramMap.get('id');
-        }),
-        switchMap((id: string) => this.userService.getUserById(id)),
-        takeUntilDestroyed(this.destroy)
-      )
-      .subscribe({
-        next: (user) => {
-          this.user = user;
-          return user;
-        },
-        error: (_err) => {
-          this.error = {
-            help: 'There was a problem loading the user – try again.',
-            httpResponse: _err.message,
-            message: _err.error?.title,
-          };
-        },
-        // complete: () => console.log('We got a new user, and we are done!'),
-      });
-  }
 }
