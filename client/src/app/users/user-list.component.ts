47d2b51bb7dfc1536566f241a88a814d409898fe
Michael Small <chickenhat22@gmail.com>
2024-08-31 12:31:58 -0500

    refactor: pure RXJS user-list

--- client/src/app/users/user-list.component.ts
+++ client/src/app/users/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,10 +12,11 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, filter, map, of, switchMap, tap } from 'rxjs';
 import { User, UserRole } from './user';
 import { UserCardComponent } from './user-card.component';
 import { UserService } from './user.service';
+import { AsyncPipe } from '@angular/common';
 
 /**
  * A component that displays a list of users, either as a grid
@@ -33,101 +34,59 @@ import { UserService } from './user.service';
     styleUrls: ['./user-list.component.scss'],
     providers: [],
     standalone: true,
-    imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, MatSelectModule, MatOptionModule, MatRadioModule, UserCardComponent, MatListModule, RouterLink, MatButtonModule, MatTooltipModule, MatIconModule]
+    imports: [AsyncPipe, MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, MatSelectModule, MatOptionModule, MatRadioModule, UserCardComponent, MatListModule, RouterLink, MatButtonModule, MatTooltipModule, MatIconModule]
 })
 
-export class UserListComponent implements OnInit, OnDestroy  {
-  // These are public so that tests can reference them (.spec.ts)
-  public serverFilteredUsers: User[];
-  public filteredUsers: User[];
+export class UserListComponent {
+    public userName$ = new BehaviorSubject<string | undefined>(undefined)
+    public userAge$ = new BehaviorSubject<number | undefined>(undefined)
+    public userRole$ = new BehaviorSubject<UserRole | undefined>(undefined)
+    public userCompany$ = new BehaviorSubject<string | undefined>(undefined)
 
-  public userName: string;
-  public userAge: number;
-  public userRole: UserRole;
-  public userCompany: string;
-  public viewType: 'card' | 'list' = 'card';
+    public $viewType = signal<'card' | 'list'>('card');
 
-  errMsg = '';
-  private ngUnsubscribe = new Subject<void>();
+    $errMsg = signal<string | undefined>(undefined);
+
+    /**
+     * This constructor injects both an instance of `UserService`
+     * and an instance of `MatSnackBar` into this component.
+     * `UserService` lets us interact with the server.
+     *
+     * @param userService the `UserService` used to get users from the server
+     * @param snackBar the `MatSnackBar` used to display feedback
+     */
+    constructor(private userService: UserService, private snackBar: MatSnackBar) {
+        // Nothing here – everything is in the injection parameters.
+    }
 
 
-  /**
-   * This constructor injects both an instance of `UserService`
-   * and an instance of `MatSnackBar` into this component.
-   * `UserService` lets us interact with the server.
-   *
-   * @param userService the `UserService` used to get users from the server
-   * @param snackBar the `MatSnackBar` used to display feedback
-   */
-  constructor(private userService: UserService, private snackBar: MatSnackBar) {
-    // Nothing here – everything is in the injection parameters.
-  }
+    serverFilteredUsers$ = combineLatest([this.userRole$, this.userAge$]).pipe(
+        switchMap(([role, age]) => this.userService.getUsers({
+            role,
+            age
+        })
+        )).pipe(
+            catchError((err) => {
+                if (err.error instanceof ErrorEvent) {
+                    this.$errMsg.set(`Problem in the client – Error: ${err.error.message}`);
+                } else {
+                    this.$errMsg.set( `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`)
+                }
+                this.snackBar.open(
+                    this.$errMsg(),
+                    'OK',
+                    { duration: 6000 });
+                return of<User[]>([])
+            }),
+            tap(() => {
+                console.log('Users were filtered on the server')
+            })
+        )
 
-  /**
-   * Get the users from the server, filtered by the role and age specified
-   * in the GUI.
-   */
-  getUsersFromServer(): void {
-    // A user-list-component is paying attention to userService.getUsers
-    // (which is an Observable<User[]>)
-    // (for more on Observable, see: https://reactivex.io/documentation/observable.html)
-    // and we are specifically watching for role and age whenever the User[] gets updated
-    this.userService.getUsers({
-      role: this.userRole,
-      age: this.userAge
-    }).pipe(
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe({
-      // Next time we see a change in the Observable<User[]>,
-      // refer to that User[] as returnedUsers here and do the steps in the {}
-      next: (returnedUsers) => {
-        // First, update the array of serverFilteredUsers to be the User[] in the observable
-        this.serverFilteredUsers = returnedUsers;
-        // Then update the filters for our client-side filtering as described in this method
-        this.updateFilter();
-      },
-      // If we observe an error in that Observable, put that message in a snackbar so we can learn more
-      error: (err) => {
-        if (err.error instanceof ErrorEvent) {
-          this.errMsg = `Problem in the client – Error: ${err.error.message}`;
-        } else {
-          this.errMsg = `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`;
-        }
-        this.snackBar.open(
-          this.errMsg,
-          'OK',
-          // The message will disappear after 6 seconds.
-          { duration: 6000 });
-      },
-      // Once the observable has completed successfully
-      // complete: () => console.log('Users were filtered on the server')
-    });
-  }
 
-  /**
-   * Called when the filtering information is changed in the GUI so we can
-   * get an updated list of `filteredUsers`.
-   */
-  public updateFilter(): void {
-    this.filteredUsers = this.userService.filterUsers(
-      this.serverFilteredUsers, { name: this.userName, company: this.userCompany });
-  }
-
-  /**
-   * Starts an asynchronous operation to update the users list
-   *
-   */
-  ngOnInit(): void {
-    this.getUsersFromServer();
-  }
-
-  /**
-   * When this component is destroyed, we should unsubscribe to any
-   * outstanding requests.
-   */
-  ngOnDestroy() {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
-  }
-
+    filteredUsers$ = combineLatest([this.serverFilteredUsers$, this.userName$, this.userCompany$]).pipe(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        filter(([serverFilteredUsers, userName, userCompany]) => serverFilteredUsers.length > 0),
+        map(([serverFilteredUsers, userName, userCompany]) => this.userService.filterUsers(serverFilteredUsers, { name: userName, company: userCompany }))
+    )
 }
