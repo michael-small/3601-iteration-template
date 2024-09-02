132f7fc611879081d67a85785af5e705359286b1
Michael Small <chickenhat22@gmail.com>
2024-09-02 14:53:44 -0500

    refactor: declarative `user` in user-profile

--- client/src/app/users/user-profile.component.ts
+++ client/src/app/users/user-profile.component.ts
@@ -1,12 +1,13 @@
-import { Component, DestroyRef, OnInit } from '@angular/core';
+import { Component, DestroyRef, Signal, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
+import { catchError, finalize, map, switchMap } from 'rxjs/operators';
 import { User } from './user';
 import { UserCardComponent } from './user-card.component';
 import { UserService } from './user.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-user-profile',
@@ -15,9 +16,8 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     standalone: true,
     imports: [UserCardComponent, MatCardModule]
 })
-export class UserProfileComponent implements OnInit {
-  user: User;
-  error: { help: string, httpResponse: string, message: string };
+export class UserProfileComponent {
+  error = signal<{ help: string, httpResponse: string, message: string } | undefined>(undefined);
 
   // This `Subject` will only ever emit one (empty) value when
   // `ngOnDestroy()` is called, i.e., when this component is
@@ -26,8 +26,7 @@ export class UserProfileComponent implements OnInit {
 
   constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private userService: UserService, private destroyRef: DestroyRef) { }
 
-  ngOnInit(): void {
-    // The `map`, `switchMap`, and `takeUntil` are all RXJS operators, and
+    // `map` and `switchMap` are RXJS operators, and
     // each represents a step in the pipeline built using the RXJS `pipe`
     // operator.
     // The map step takes the `ParamMap` from the `ActivatedRoute`, which
@@ -37,39 +36,32 @@ export class UserProfileComponent implements OnInit {
     // That ID string gets passed (by `pipe`) to `switchMap`, which transforms
     // it into an Observable<User>, i.e., all the (zero or one) `User`s
     // that have that ID.
-    // The `takeUntil` operator allows this pipeline to continue to emit values
-    // until `this.ngUnsubscribe` emits a value, saying to shut the pipeline
-    // down and clean up any associated resources (like memory).
-    this.route.paramMap.pipe(
-      // Map the paramMap into the id
-      map((paramMap: ParamMap) => paramMap.get('id')),
-      // Maps the `id` string into the Observable<User>,
-      // which will emit zero or one values depending on whether there is a
-      // `User` with that ID.
-      switchMap((id: string) => this.userService.getUserById(id)),
-      // Allow the pipeline to continue to emit values until the component is destroyed
-      // At that point we shut down the pipeline, allowed any
-      // associated resources (like memory) are cleaned up.
-      takeUntilDestroyed(this.destroyRef)
-    ).subscribe({
-      next: user => {
-        this.user = user;
-        return user;
-      },
-      error: _err => {
-        this.error = {
-          help: 'There was a problem loading the user – try again.',
-          httpResponse: _err.message,
-          message: _err.error?.title,
-        };
-      }
-      /*
-       * You can uncomment the line that starts with `complete` below to use that console message
-       * as a way of verifying that this subscription is completing.
-       * We removed it since we were not doing anything interesting on completion
-       * and didn't want to clutter the console log
-       */
-      // complete: () => console.log('We got a new user, and we are done!'),
-    });
-  }
+    user: Signal<User | undefined> = toSignal(
+        this.route.paramMap.pipe(
+            // Map the paramMap into the id
+            map((paramMap: ParamMap) => paramMap.get('id')),
+            // Maps the `id` string into the Observable<User>,
+            // which will emit zero or one values depending on whether there is a
+            // `User` with that ID.
+            switchMap((id: string) => this.userService.getUserById(id)),
+            catchError(_err => {
+                this.error.set( {
+                    help: 'There was a problem loading the user – try again.',
+                    httpResponse: _err.message,
+                    message: _err.error?.title,
+                })
+                // Something has to be returned to keep the shape of the user right
+                // `of` just says something should be treated as an observable
+                // You can use `of` elsewhere to mock out a value as an observable
+                return of<undefined>()
+            }),
+            /*
+            * You can uncomment the line that starts with `finalize` below to use that console message
+            * as a way of verifying that this subscription is completing.
+            * We removed it since we were not doing anything interesting on completion
+            * and didn't want to clutter the console log
+            */
+            finalize(() => console.log('We got a new user, and we are done!'))
+        ),
+    )
 }
